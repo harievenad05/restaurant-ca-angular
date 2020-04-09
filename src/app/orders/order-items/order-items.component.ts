e3e85b24976c2d2f5d947ae4ff59f68c450e8780
VidhyaRiya <vidhyariya@Haries-Mac-mini.local>
2020-04-10 00:36:02 +0300

    form submit validation done

--- src/app/orders/order-items/order-items.component.ts
+++ src/app/orders/order-items/order-items.component.ts
@@ -3,6 +3,8 @@ import { MatDialogRef, MAT_DIALOG_DATA } from '@angular/material/dialog';
 import { OrderItem } from '../../shared/order-item.model';
 import { ItemService } from '../../shared/item.service';
 import { Item, ItemData } from '../../shared/item.model';
+import { NgForm } from '@angular/forms';
+import { OrderService } from '../../shared/order.service';
 
 @Component({
   selector: 'app-order-items',
@@ -14,10 +16,13 @@ export class OrderItemsComponent implements OnInit {
   formData: OrderItem;
   dataReceived: ItemData;
   itemList: Item[];
+  isValid: boolean = true;
+
   constructor(
     @Inject(MAT_DIALOG_DATA) public data,
     public dialogRef: MatDialogRef<OrderItemsComponent>,
-    private itemService: ItemService
+    private itemService: ItemService,
+    private orderService: OrderService
   ) { }
 
   ngOnInit(): void {
@@ -39,6 +44,39 @@ export class OrderItemsComponent implements OnInit {
       item_price: 0,
       total: 0
     }
+  }
+
+  updatePriceAction(event){
+    if(event.selectedIndex == 0){
+      this.formData.item_price = 0;
+      this.formData.item_name = '';
+    } else {
+      this.formData.item_price = this.itemList[event.selectedIndex-1].item_price;
+      this.formData.item_name = this.itemList[event.selectedIndex-1].item_name;
+    }
+    this.updateTotal();
+  }
+
+  updateTotal(){
+    this.formData.total = parseFloat((this.formData.quantity * this.formData.item_price).toFixed(2));
+  }
+
+  onSubmitAction(form: NgForm){
+    if(this.validateForm(form.value)){
+      this.orderService.orderItems.push(form.value);
+      this.dialogRef.close();
+    }
+  }
+
+  validateForm(formData: OrderItem){
+    this.isValid = true;
+    if(formData.item_id == 0){
+      this.isValid = false;
+    } else if(formData.quantity == 0){
+      this.isValid = false;
+    }
+
+    return this.isValid
   }
 
 }
